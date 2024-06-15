05904c8a59d05d4275cb8a85116affce30fb2218
guskirb <anguskirby1998@gmail.com>
2024-06-15 11:21:00 +0100

    Set up source mapping

--- controllers/postController.ts
+++ controllers/postController.ts
@@ -4,6 +4,7 @@ import asyncHandler from "express-async-handler";
 const postController = (() => {
   const post_get = asyncHandler(async (req, res, next) => {
     const posts = await Post.find().exec();
+    console.log(req);
     res.status(200).json(posts);
   });
 
