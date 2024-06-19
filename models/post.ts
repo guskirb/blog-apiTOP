eb111db5e3454f4eb72d2d1ad7cfb31573e8bd57
guskirb <anguskirby1998@gmail.com>
2024-06-19 09:14:57 +0100

    Add luxon and format post date in schema

--- models/post.ts
+++ models/post.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
+import { DateTime } from "luxon";
 
 const PostSchema = new Schema({
   title: { type: String, required: true },
@@ -8,6 +9,10 @@ const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   date: { type: Date, default: Date.now },
 });
+
+PostSchema.virtual('date_formatted').get(function() {
+  return  DateTime.fromJSDate(this.date).toISODate(DateTime.DATE_FULL);
+})
 
 const PostModel = mongoose.model("Post", PostSchema);
 export default PostModel;
